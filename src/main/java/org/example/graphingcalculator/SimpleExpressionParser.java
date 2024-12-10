43521930431d200eb73a6dfbdf352c043c90dbc2
Neel Awsare <neelawsare@gmail.com>
2024-12-09 23:06:00 -0500

    Update SimpleExpressionParser.java

--- src/main/java/org/example/graphingcalculator/SimpleExpressionParser.java
+++ src/main/java/org/example/graphingcalculator/SimpleExpressionParser.java
@@ -66,7 +66,7 @@ public class SimpleExpressionParser implements ExpressionParser {
 					return null;
 				}
 
-				return new AdditionCompoundExpression(new Expression[]{left, right});
+				return new AdditionCompoundExpression(left, right);
 			} else if (str.charAt(i) == '-') {
 				Expression left = validateExpression(str.substring(0, i));
 				Expression right = validateExpression(str.substring(i+1));
@@ -75,7 +75,7 @@ public class SimpleExpressionParser implements ExpressionParser {
 					return null;
 				}
 
-				return new SubtractionCompoundExpression(new Expression[]{left, right});
+				return new SubtractionCompoundExpression(left, right);
 			}
 		}
 
@@ -92,7 +92,7 @@ public class SimpleExpressionParser implements ExpressionParser {
 					return null;
 				}
 
-				return new MultiplicationCompoundExpression(new Expression[]{left, right});
+				return new MultiplicationCompoundExpression(left, right);
 			} else if (str.charAt(i) == '/') {
 				Expression numerator = validateExpression(str.substring(0, i));
 				Expression denominator = validateExpression(str.substring(i+1));
@@ -101,7 +101,7 @@ public class SimpleExpressionParser implements ExpressionParser {
 					return null;
 				}
 
-				return new DivisionCompoundExpression(new Expression[]{numerator, denominator});
+				return new DivisionCompoundExpression(numerator, denominator);
 			}
 		}
 
